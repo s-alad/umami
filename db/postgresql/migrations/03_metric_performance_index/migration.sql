4a67b7562c1a9c34228808ddc1d3ee10b0a11ee2
s-alad <n7.saad@gmail.com>
2024-03-13 00:00:14 -0400

    local fix

--- db/postgresql/migrations/03_metric_performance_index/migration.sql
+++ db/postgresql/migrations/03_metric_performance_index/migration.sql
@@ -1,26 +1,26 @@
 -- CreateIndex
-CREATE INDEX "event_data_website_id_created_at_idx" ON "event_data"("website_id", "created_at");
+-- CREATE INDEX "event_data_website_id_created_at_idx" ON "event_data"("website_id", "created_at");
 
 -- CreateIndex
-CREATE INDEX "event_data_website_id_created_at_event_key_idx" ON "event_data"("website_id", "created_at", "event_key");
+-- CREATE INDEX "event_data_website_id_created_at_event_key_idx" ON "event_data"("website_id", "created_at", "event_key");
 
 -- CreateIndex
-CREATE INDEX "session_website_id_created_at_idx" ON "session"("website_id", "created_at");
+-- CREATE INDEX "session_website_id_created_at_idx" ON "session"("website_id", "created_at");
 
 -- CreateIndex
-CREATE INDEX "session_website_id_created_at_hostname_idx" ON "session"("website_id", "created_at", "hostname");
+-- CREATE INDEX "session_website_id_created_at_hostname_idx" ON "session"("website_id", "created_at", "hostname");
 
 -- CreateIndex
-CREATE INDEX "session_website_id_created_at_browser_idx" ON "session"("website_id", "created_at", "browser");
+-- CREATE INDEX "session_website_id_created_at_browser_idx" ON "session"("website_id", "created_at", "browser");
 
 -- CreateIndex
-CREATE INDEX "session_website_id_created_at_os_idx" ON "session"("website_id", "created_at", "os");
+-- CREATE INDEX "session_website_id_created_at_os_idx" ON "session"("website_id", "created_at", "os");
 
 -- CreateIndex
-CREATE INDEX "session_website_id_created_at_device_idx" ON "session"("website_id", "created_at", "device");
+-- CREATE INDEX "session_website_id_created_at_device_idx" ON "session"("website_id", "created_at", "device");
 
 -- CreateIndex
-CREATE INDEX "session_website_id_created_at_screen_idx" ON "session"("website_id", "created_at", "screen");
+/* CREATE INDEX "session_website_id_created_at_screen_idx" ON "session"("website_id", "created_at", "screen");
 
 -- CreateIndex
 CREATE INDEX "session_website_id_created_at_language_idx" ON "session"("website_id", "created_at", "language");
@@ -48,3 +48,4 @@ CREATE INDEX "website_event_website_id_created_at_page_title_idx" ON "website_ev
 
 -- CreateIndex
 CREATE INDEX "website_event_website_id_created_at_event_name_idx" ON "website_event"("website_id", "created_at", "event_name");
+ */
